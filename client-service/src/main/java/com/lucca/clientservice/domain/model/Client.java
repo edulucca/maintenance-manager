1836143f6cd7a3fccc7896e8bd16ac737a15d5b0
Eduardo de Lucca <eduardo.luccati@gmail.com>
2026-03-17 16:12:05 -0300

    Client Service - prevenção contra NullPointerException

--- client-service/src/main/java/com/lucca/clientservice/domain/model/Client.java
+++ client-service/src/main/java/com/lucca/clientservice/domain/model/Client.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -26,5 +27,6 @@ public class Client {
 
     private String email;
 
-    private List<String> vehicles;
+    @Builder.Default
+    private List<String> vehicles = new ArrayList<>();
 }
